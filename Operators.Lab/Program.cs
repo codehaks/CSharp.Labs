fadd4ed5f0a434466fd00bb55aab91aa5e161cc5
Hakim Ghods <ghods.hakim@hotmail.com>
2018-11-12 01:31:49 +0330

    Add project files.

---
+++ Operators.Lab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Operators.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+
+            int y = x ?? -1;
+
+            // y=-1
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
