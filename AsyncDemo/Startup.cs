b5c5eb559f20af6d978409337a7417d99d8057f8
Hakim Ghods <ghods.hakim@hotmail.com>
2019-02-07 21:50:56 +0330

    Work

---
+++ AsyncDemo/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AsyncDemo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+
+                var t1 =  Work1();
+                var t2 =  Work2();
+
+                var t3 = await Work3();
+                var t4 = await Work4();
+
+                await context.Response.WriteAsync($"\n Work1 -> Thread : {t1.Result}");
+                await context.Response.WriteAsync($"\n Work1 -> Thread : {t2.Result}");
+            });
+        }
+
+
+        public async Task<int> Work1()
+        {
+            await Task.Delay(5000);
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public async Task<int> Work2()
+        {
+            await Task.Delay(5000);
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public async Task<int> Work3()
+        {
+            await Task.Delay(2000);
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public async Task<int> Work4()
+        {
+            await Task.Delay(2000);
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+
+    }
+}
---
+++ DelegateDemo2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DelegateDemo2
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+
+        static void Main(string[] args)
+        {
+            var x = 2;
+            var y = 3;
+
+            var calc = new Operation(Multiply);
+            calc += new Operation(Add);
+
+
+            var result = calc(x, y);
+
+            Func<int, int, int> f1 = delegate(int x1,int y1) { return x1 - y1; } ;
+
+            Func<int, int, int> f2 = (x2, y2) =>  x2 + y2;
+
+            Action<int> a1 = x3 => Console.WriteLine(x3);
+            a1.Invoke(5);
+
+
+            result =f1(4, 5);
+            
+            Console.WriteLine($" Result : {result}");
+        }
+
+        static public int Add(int x,int y)
+        {
+            Console.WriteLine("1");
+            return x + y;
+        }
+
+        static public int Multiply(int x, int y)
+        {
+            Console.WriteLine("2");
+            return x * y;
+        }
+    }
+}
